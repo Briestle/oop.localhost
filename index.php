aac04c6093529b726c54fe686877a9478855448f
Kostya Baetyl <baetulkostua@gmail.com>
2020-07-16 22:47:04 +0300

    dsf
    sdf

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 
 
 <?php
+//наследование
 
-class Animal {
+/*class Animal {
     public $tail;
     public $paws;
     public $nose;
@@ -70,15 +71,79 @@ class Cat extends Animal {
 
 }
 
+
+$cat = new Cat("little ", "big ", "small ", "black ", "sly");
+echo '<pre>';
+echo'Cat:  ';
+echo $cat->getInfo();
+echo '</pre>';*/
+
+
+//абстракция 
+abstract class Animal {
+    public $tail;
+    public $paws;
+    public $nose;
+    public $eyes;
+
+   public function __construct($tail, $paws, $nose, $eyes) {
+        $this->tail = $tail;
+        $this->paws = $paws;
+        $this->nose = $nose;
+        $this->eyes = $eyes;
+    }
+
+   abstract function getInfo();
+}
+
+
+
+class Dog extends Animal {
+    private $power;
+
+    function __construct($tail, $paws, $nose, $eyes, $power)
+    {
+        parent::__construct($tail, $paws, $nose, $eyes);
+        $this->power = $power;
+    }
+
+    function getInfo() {
+        $info = "{$this->tail}"."{$this->paws}"."{$this->nose}"."{$this->eyes}";
+        $info .= "{$this->power}";
+        return $info;
+    }
+
+}
+
+$dog = new Dog("little ", "big ", "small ", "black ", "full");
+echo '<pre>';
+echo'Dog:  ';
+echo $dog->getInfo();
+echo '</pre>';
+
+
+class Cat extends Animal {
+    private $sly;
+
+    function __construct($tail, $paws, $nose, $eyes, $sly)
+    {
+        parent::__construct($tail, $paws, $nose, $eyes);
+        $this->sly = $sly;
+    }
+
+    function getInfo() {
+        $info = "{$this->tail}"."{$this->paws}"."{$this->nose}"."{$this->eyes}";
+        $info .= "{$this->sly}";
+        return $info;
+    }
+
+}
+
 $cat = new Cat("little ", "big ", "small ", "black ", "sly");
 echo '<pre>';
 echo'Cat:  ';
 echo $cat->getInfo();
 echo '</pre>';
-
-
-
-
 
 ?>
 
